b939e73c2519fe0b1ee3ed184c06951aa88d0e3d
Sachindra247 <srimalsachindra@gmail.com>
2025-03-10 06:01:19 -0400

    Power bi home page final attempt 102

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -112,7 +112,7 @@ const HomePage = () => {
     const fetchEmbedToken = async () => {
       try {
         const response = await axios.get(
-          "https://powerbi-backend-g5g5p7h1n-sachindras-projects-95cdce12.vercel.app/api/powerbi/embed-token"
+          "https://powerbi-backend-agl02cdaw-sachindras-projects-95cdce12.vercel.app/api/powerbi/embed-token"
         );
         console.log("Embed Token Response:", response.data); // Log the response
         setEmbedToken(response.data.embedToken); // Assuming the response contains { embedToken: "YOUR_TOKEN" }
