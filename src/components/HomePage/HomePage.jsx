2b14c92f480402be39f9e6f5af095e05e08c0065
Sachindra247 <srimalsachindra@gmail.com>
2025-03-13 01:56:24 -0400

    Power bi page route fix attempt 10

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -132,6 +132,12 @@ const reports = {
       "https://app.powerbi.com/reportEmbed?reportId=a1e79c84-1882-47af-a853-8fe202696ee4&groupId=8a6e72c9-e6d2-4c79-8ea1-41b4994c811f",
     pageId: "your-feedback-page-id-here", // Replace with the actual page ID for Feedback
   },
+  healthscore: {
+    id: "a1e79c84-1882-47af-a853-8fe202696ee4",
+    embedUrl:
+      "https://app.powerbi.com/reportEmbed?reportId=a1e79c84-1882-47af-a853-8fe202696ee4&groupId=8a6e72c9-e6d2-4c79-8ea1-41b4994c811f",
+    pageId: "your-healthscore-page-id-here", // Replace with the actual page ID for Health Score
+  },
 };
 
 const HomePage = () => {
