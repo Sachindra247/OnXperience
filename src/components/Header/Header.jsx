1e0b48373d189fc59a3661a1400109f3e238314d
Sachindra247 <srimalsachindra@gmail.com>
2025-04-01 16:04:09 -0400

    logo linked to homepage

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,15 @@
-// src/components/Header/Header.jsx
 import React from "react";
+import { Link } from "react-router-dom"; // Import Link
 import Logo from "../Assets/logo.png";
-import SearchBar from "../SearchBar/SearchBar"; // Import the SearchBar component
+import SearchBar from "../SearchBar/SearchBar";
 
 const Header = ({ onSearch }) => {
   return (
     <header className="header">
-      <img src={Logo} alt="Logo" className="logo" />
-      <SearchBar onSearch={onSearch} /> {/* Use the SearchBar component */}
+      <Link to="/">
+        <img src={Logo} alt="Logo" className="logo" />
+      </Link>
+      <SearchBar onSearch={onSearch} />
     </header>
   );
 };
