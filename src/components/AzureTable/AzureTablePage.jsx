43f586d6f6aaff835abcd8279e856331f1759852
Sachindra247 <srimalsachindra@gmail.com>
2025-04-21 20:17:26 -0400

    user data validations 3

--- src/components/AzureTable/AzureTablePage.jsx
+++ src/components/AzureTable/AzureTablePage.jsx
@@ -29,7 +29,7 @@ const AzureTablePage = () => {
       ...prev,
       [id]: {
         ...prev[id],
-        [field]: value,
+        [field]: value === "" ? "" : Number(value),
       },
     }));
   };
